2b143a3bac99b829a242e0db19218f380dcd5e56
ivankenou <ivan.kenou.ngnentedem@mni.thm.de>
2020-10-22 19:03:47 +0200

    first java program

--- Helloword.java
+++ Helloword.java
@@ -4,5 +4,7 @@
   public static void main (String[] args)
   {
    System.out.println("hello word");
+   System.out.println("hier ist mein erstes Programm, es ist noch nicht fertig , aber wird bald später optional danke");
+   
   }
  } 
